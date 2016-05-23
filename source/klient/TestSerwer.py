7c142bdf0435205c46fd7ca017cc048ae730cdd4
mhaponiu <m.haponiuk@stud.elka.pw.edu.pl>
2016-05-23 11:41:12 +0200

    początkowe api klient + zalążek serwera testowego

--- source/klient/TestSerwer.py
+++ source/klient/TestSerwer.py
@@ -1,17 +1,29 @@
 from flask import Flask
+from multiprocessing import Process
 
-class TestSerwerWWW(object):
-    app = Flask(__name__)
-    def __init__(self):
-        self.aaa = 'aaa'
+class TestKlientSerwerWWW(object):
+    app = Flask("APP")
+    def __init__(self, text, port=5000):
+        # self.app = app = Flask(__name__)
+        self.port = port
+        self.text = text
 
     def run(self):
-        self.app.run(host='0.0.0.0', debug=True)
+        self.app.run(host='0.0.0.0', debug=True, port=self.port)
 
     @app.route('/')
     def hello_word():
-        return 'hello'
+        print("przetwarzam request'a")
+        return "hello word"
+
+# class TestKlientSerwerWWW2(TestKlientSerwerWWW):
 
 if __name__=='__main__':
-    www = TestSerwerWWW()
-    www.run()
+    www = TestKlientSerwerWWW(text="jeden", port=5000)
+    www2 = TestKlientSerwerWWW(text="dwa", port=5001)
+    p1 = Process(target=www.run)
+    p2 = Process(target=www2.run)
+    p1.start()
+    p2.start()
+    p1.join()
+    p2.join()
--- source/klient/klient.py
+++ source/klient/klient.py
@@ -1,6 +1,17 @@
+import requests
+
 class Klient(object):
-    def __init__(self):
-        self.token=None
+    def __init__(self, plik_konfiguacyjny):
+        self.plik_konf = plik_konfiguacyjny
+        self.token = None
+        self.timeout = None
+        self.listaIP = None
+
+        self._wczytajlistaIP()
+
+    def _wczytajlistaIP(self):
+        # parsowanie pliku
+        self.listaIP = ['127.0.0.1:5000', '127.0.0.1:5001']
 
     def zaloguj(self):
         pass
@@ -23,5 +34,9 @@ class Klient(object):
     def listaPlikowZnajomego(self, znajomy):
         pass
 
-    def wczytajListeIP(self):
-        pass
+
+
+if __name__ == '__main__':
+    klient = Klient("sciezka_plik_konfiguracyjny")
+
+    klient
