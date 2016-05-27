f2394370bd8a3ea871523be43b73eaba2c66578b
Szop-kradziej <karolina.drabarz@gmail.com>
2016-05-27 22:38:11 +0200

    Add getters to modules configurations

--- etap2/configuration_module/src/parser.py
+++ etap2/configuration_module/src/parser.py
@@ -4,11 +4,16 @@ class Parser (object):
     def __init__(self, path="../../config.yml"):
         with open(path, 'r') as stream:
             try:
-                print(yaml.load(stream))
+                self.configuration = yaml.load(stream)
             except yaml.YAMLError as exc:
                 print(exc)
                 
-if __name__ == '__main__':
-    Parser()
-    path = "../../config.yml"
-    Parser(path)
+    def getLoadbalancerConfiguration (self):
+        return self.configuration["loadbalancer_conf"]
+    
+    def getServerConfiguration (self):
+        return self.configuration["api_server"]
+    
+    def getDistributedDBConfiguration (self):
+        return self.configuration["distributed_db"]
+                
