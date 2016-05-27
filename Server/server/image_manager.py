b408ec965b402127ff53b8b1b519b8e443d6861a
Michał Chilczuk <michal.chilczuk@gmail.com>
2016-05-27 07:23:54 +0200

    stub image manager

---
+++ Server/server/image_manager.py
@@ -0,0 +1,18 @@
+class ImageManager(object):
+    def get_images(self, user_id):
+        return 0;
+    
+    def get_image(self, image_id):
+        return id;
+        
+    def upload_image(self, image):
+        return 0;
+    
+    def delete_image(self, image_id):
+        return 0;
+    
+    def share_image(self, image_id, user_id):
+        return 0;
+    
+    def unshare_image(self, image_id, user_id):
+        return 0;
