88344a771259b71a0e291a8ffad013e90abcc923
Szop-kradziej <karolina.drabarz@gmail.com>
2016-06-09 20:48:20 +0200

    Add get user method

--- etap2/database_mockup/src/database_mockup.py
+++ etap2/database_mockup/src/database_mockup.py
@@ -1,10 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort, make_response
 
 database_mockup = Flask(__name__)
 
 users = [
-         {
-        'John_hash': {
+         { 'user_hash': 'John_hash',
+          'user_data': {
                       'user_name': 'John',
                       'user_password': 'pass1',
                       'friends': [{ 
@@ -18,8 +18,8 @@ users = [
                                   'photo_hash': 'hash2'
                                   }]
                       }},
-         {
-        'Eric_hash': {
+         { 'user_hash': 'Eric_hash',
+        'user_data': {
                       'user_name': 'Eric',
                       'user_password': 'pass2',
                       'friends': [{ 
@@ -34,8 +34,8 @@ users = [
                                   'photo_hash': 'hash3'
                                   }]
                       }},
-          {
-        'Ana_hash': {
+          { 'user_hash' : 'Ana_hash',
+        'user_data': {
                      'user_name': 'Ana',
                      'user_password': 'pass3',
                      'images': [{
@@ -53,8 +53,20 @@ def index():
     return jsonify({'users': users})
 
 @database_mockup.route('/rso/users/', methods=['GET'])
-def get_tasks():
+def get_users():
     return jsonify({'users': users})
 
+@database_mockup.route('/rso/users/<string:user_hash>', methods=['GET'])
+def get_user(user_hash):
+    user = [user for user in users if user['user_hash'] == user_hash]
+    print (user)
+    if len(user) == 0:
+        abort(404)
+    return jsonify(user)
+
+@database_mockup.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
 if __name__ == '__main__':
-    database_mockup.run(debug=True)
+    database_mockup.run(debug=True, host="0.0.0.0")
