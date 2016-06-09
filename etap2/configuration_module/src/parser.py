22c33933e6c369424cbe6ace80b33d351e165d2d
Szop-kradziej <karolina.drabarz@gmail.com>
2016-06-09 11:42:05 +0200

    Add server api configuration getters

--- etap2/configuration_module/src/parser.py
+++ etap2/configuration_module/src/parser.py
@@ -16,4 +16,42 @@ class Parser (object):
     
     def get_distributed_db_configuration (self):
         return self.configuration["distributed_db"]
-            
+    
+    def get_db_host(self):
+        return self.get_server_configuration()["db_host"]
+    
+    def get_db_port(self):
+        return self.get_server_configuration()["db_port"]
+    
+    def get_db_db(self):
+        return self.get_server_configuration()["db_db"]
+    
+def test():
+    parser = Parser("test_config.yml")
+    get_db_host_test(parser)
+    get_db_port_test(parser)
+    get_db_db_test(parser)
+
+def get_db_host_test(parser):
+    if not '192.168.56.2' == parser.get_db_host() :
+        print ("DB host should be returned correctly")
+        return -1
+    print ("OK")
+    return 0
+
+def get_db_port_test(parser):
+    if not 6379 == parser.get_db_port() :
+        print ("DB port should be returned correctly")
+        return -1
+    print ("OK")
+    return 0
+
+def get_db_db_test(parser):
+    if not 0 == parser.get_db_db() :
+        print ("DB db should be returned correctly")
+        return -1
+    print ("OK")
+    return 0
+
+if __name__ == '__main__':
+    test()
