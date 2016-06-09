8165156b9bd11eae0e4dab1fbe39e998add12f79
Szop-kradziej <karolina.drabarz@gmail.com>
2016-06-09 15:56:22 +0200

    Add client getters for load balancers info

--- etap2/configuration_module/src/parser.py
+++ etap2/configuration_module/src/parser.py
@@ -9,13 +9,19 @@ class Parser (object):
                 print(exc)
                 
     def get_load_balancer_configuration (self):
-        return self.configuration["loadbalancer_conf"]
+        return self.configuration["load_balancer"]
     
     def get_server_configuration (self):
-        return self.configuration["api_server"]
+        return self.configuration["server"]
     
     def get_distributed_db_configuration (self):
-        return self.configuration["distributed_db"]
+        return self.configuration["data_base"]
+    
+    def get_loadbalancer_link_name(self):
+        return self.get_load_balancer_configuration()["link"]
+    
+    def get_loadbalancers_count(self):
+        return self.get_load_balancer_configuration()["nodes_info"]["count"]
     
     def get_db_host(self):
         return self.get_server_configuration()["db_host"]
@@ -31,6 +37,8 @@ def test():
     get_db_host_test(parser)
     get_db_port_test(parser)
     get_db_db_test(parser)
+    get_loadbalancer_link_name_test(parser)
+    get_loadbalancers_count_test(parser)
 
 def get_db_host_test(parser):
     if not '192.168.56.2' == parser.get_db_host() :
@@ -49,6 +57,20 @@ def get_db_port_test(parser):
 def get_db_db_test(parser):
     if not 0 == parser.get_db_db() :
         print ("DB db should be returned correctly")
+        return -1
+    print ("OK")
+    return 0
+
+def get_loadbalancer_link_name_test(parser):
+    if not 'loadbalancer' == parser.get_loadbalancer_link_name() :
+        print ("Loadbalancer link name should be returned correctly")
+        return -1
+    print ("OK")
+    return 0
+
+def get_loadbalancers_count_test(parser):
+    if not 3 == parser.get_loadbalancers_count() :
+        print ("Loadbalancers count should be returned correctly")
         return -1
     print ("OK")
     return 0
